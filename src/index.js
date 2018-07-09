b77bfd010cbb5632fdbc7d02b9821e4a0613b166
Utwo <mihai.legat@gmail.com>
2018-07-09 18:01:14 +0300

    Add js and json lint. Move all source code to src

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const definitions = require('./definitions/definitions.json')
+const mappingHtml = require('./definitions/mapping-html.json')
+const mappingReact = require('./definitions/mapping-react.json')
+const mappingReactNative = require('./definitions/mapping-react-native.json')
+const mappingNext = require('./definitions/mapping-next.json')
+const mappingNuxt = require('./definitions/mapping-nuxt.json')
+const mappingVue = require('./definitions/mapping-vue.json')
+
+const core = {
+  definitions,
+  mappingHtml,
+  mappingReact,
+  mappingReactNative,
+  mappingNext,
+  mappingNuxt,
+  mappingVue,
+}
+
+module.exports = core
